82aa885847b42b88e298da9350455046c688261e
Aanya-Garg06 <196170280+Aanya-Garg06@users.noreply.github.com>
2025-02-02 13:35:48 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int n,m;
+    char l;
+    scanf("%d %d %c",&n,&m,&l);
+    if (l=="+"){
+        printf("%d",n+m);
+    }
+    else if(l=="-"){
+        printf("%d",n-m);
+    }
+    else if(l=="/"){
+        printf("%d",n/m);
+    }
+    else if(l=="*"){
+        printf("%d",n*m);
+    }
+    return 0;
+}
